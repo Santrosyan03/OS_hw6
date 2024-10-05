c46b3ce61e60e33a4746347e4b752555e62522fd
Sevak <sevaksantrosyan5@gmail.com>
2024-10-06 01:38:51 +0400

    Operating Systems homework 6

--- task3_without_pragma_pack.c
+++ task3_without_pragma_pack.c
@@ -8,11 +8,9 @@ struct MyStruct {
 
 int main() {
     struct MyStruct s;
-
     printf("Size of struct: %lu bytes\n", sizeof(s));
     printf("Address of a: %p\n", (void*)&s.a);
     printf("Address of b: %p\n", (void*)&s.b);
-    printf("Address of c: %p\n", (void*)&s.c);
-
+    printf("Address of c: %p\n", (void*)&s.c);\
     return 0;
 }
